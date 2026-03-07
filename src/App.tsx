c04a5f64966dbf235aa3d04914124742021bcd8c
TESLA <75523193+W11TY@users.noreply.github.com>
2026-03-07 19:29:13 +0530

    Remove GitHub Pages basename for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Be-More-Human">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/product" element={<Product />} />
